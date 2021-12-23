4a3aed4de37bb6172e0a3b5e8c5bf8eaa6cca6fe
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-23 17:13:18 -0300

    feat(components): create Products

---
+++ src/components/Products/index.tsx
@@ -0,0 +1,14 @@
+import { ProductCard } from "../ProductCard";
+import { Container } from "./styles";
+
+export function Products() {
+  return (
+    <Container>
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </Container>
+  )
+}
---
+++ src/components/Products/styles.ts
@@ -0,0 +1,14 @@
+import tw from 'tailwind-styled-components';
+
+export const Container = tw.article`
+  min-h-screen
+  p-8
+
+  grid
+  grid-cols-4
+  gap-4
+
+  rounded-md
+
+  bg-gray-light
+`;
