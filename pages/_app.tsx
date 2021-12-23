ca100feb9326cfe52121e2fd80d1aeb208da04a8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-23 14:50:55 -0300

    feat(pages): render Header in all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/global.css';
 import type { AppProps } from 'next/app';
 
+import { Header } from '../src/components/Header';
+
+import '../styles/globals.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
