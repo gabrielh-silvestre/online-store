f285774d9e043e794fcf60b407300fc7cc2dabfc
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-23 14:28:27 -0300

    build(tailwind): change default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#082032',
+      gray: '#2C394B',
+      'gray-light': '#334756',
+      red: '#FF4C29',
+    },
     extend: {},
   },
   plugins: [],
