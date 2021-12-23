a91923020dee5fd56e66de480cb16b89229e121d
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-23 14:42:39 -0300

    build(tailwind): config container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,24 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
       black: '#082032',
       gray: '#2C394B',
-      'gray-light': '#334756',
+      'gray-blue': '#334756',
+      'gray-light': '#B2B1B9',
       red: '#FF4C29',
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
     },
     extend: {},
   },
