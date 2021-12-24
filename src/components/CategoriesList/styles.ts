9cea5f145230fbc856991591c6d58eb79374fef0
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-24 18:32:07 -0300

    style(CategoriesList): limit height and add hidden scroll

--- src/components/CategoriesList/styles.ts
+++ src/components/CategoriesList/styles.ts
@@ -1,12 +1,25 @@
+import styled from 'styled-components';
 import tw from 'tailwind-styled-components';
 
-export const CategoriesNav = tw.nav`
+const Nav = styled.nav`
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const CategoriesNav = tw(Nav)`
   w-48
+  max-h-96
 
   py-4
 
   flex
   flex-col
+
+  overflow-y-scroll
 
   rounded-b-md
 
