b96f0a8780f21b5d105187204448a5ac9bfd461b
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-24 18:31:33 -0300

    feat(CategoriesList): generate items based on api

--- src/components/CategoriesList/index.tsx
+++ src/components/CategoriesList/index.tsx
@@ -1,24 +1,34 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+import { apiListCategories } from '../../services/api';
+
 import { CategoriesNav, CategoryItem } from './styles';
 
+interface Categories {
+  name: string;
+  id: string;
+}
+
 export function CategoriesList() {
+  const [categories, setCategories] = useState<Categories[]>([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const res = await apiListCategories;
+      setCategories(res.data);
+    };
+
+    fetchCategories();
+  }, []);
+
   return (
     <CategoriesNav>
-      <Link passHref href={'/'}>
-        <CategoryItem>Carro</CategoryItem>
-      </Link>
-      <Link passHref href={'/'}>
-        <CategoryItem>Carro</CategoryItem>
-      </Link>
-      <Link passHref href={'/'}>
-        <CategoryItem>Carro</CategoryItem>
-      </Link>
-      <Link passHref href={'/'}>
-        <CategoryItem>Carro</CategoryItem>
-      </Link>
-      <Link passHref href={'/'}>
-        <CategoryItem>Carro</CategoryItem>
-      </Link>
+      {categories.map(({ id, name }) => (
+        <Link key={id} passHref href={`/category/${id}`}>
+          <CategoryItem>{name}</CategoryItem>
+        </Link>
+      ))}
     </CategoriesNav>
   );
 }
