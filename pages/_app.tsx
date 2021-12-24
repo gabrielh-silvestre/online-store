acf8253033e16a3db63b80d6112d3e0a554584c9
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-24 19:55:26 -0300

    feat(_app): encapsulate application with redux provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+
+import store from '../src/store';
 
 import { Header } from '../src/components/Header';
 
@@ -6,10 +9,10 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
