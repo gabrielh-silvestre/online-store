3192e219318bb401ed80a32c9d3263f40e2c2b7c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-24 18:53:34 -0300

    feat(Header): toggle list visibility

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
 import { HiShoppingCart, HiHeart, HiMenu } from 'react-icons/hi';
 import { CategoriesList } from '../CategoriesList';
 
 import { Container, ContentContainer, HeaderNav, List } from './styles';
 
 export function Header() {
+  const [isListHide, setisListHide] = useState(true);
+
+  const handleListToggle = () => {
+    setisListHide(!isListHide);
+  };
+
   return (
     <Container>
       <ContentContainer>
@@ -16,10 +24,10 @@ export function Header() {
         <HeaderNav>
           <HiHeart className="w-8 h-8 mr-8" />
           <HiShoppingCart className="w-8 h-8 mr-8" />
-          <HiMenu className="w-8 h-8" />
+          <HiMenu className="w-8 h-8" onClick={handleListToggle} />
         </HeaderNav>
 
-        <List $isHide={false}>
+        <List $isHide={isListHide}>
           <CategoriesList />
         </List>
       </ContentContainer>
