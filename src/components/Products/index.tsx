64df54b2333225cc47256c137dc5e73efa7a2c1d
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-24 20:30:50 -0300

    feat(Products): use redux state to render ProductCard

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,14 +1,21 @@
-import { ProductCard } from "../ProductCard";
-import { Container } from "./styles";
+import { useSelector } from 'react-redux';
+
+import { RootState } from '../../store';
+
+import { ProductCard } from '../ProductCard';
+
+import { Container } from './styles';
 
 export function Products() {
+  const products = useSelector(({ products }: RootState) => products.products);
+
   return (
     <Container>
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      {products.length !== 0 ? (
+        products.map((product) => <ProductCard key={product.id} {...product} />)
+      ) : (
+        <h3>Pesquise algo</h3>
+      )}
     </Container>
-  )
+  );
 }
