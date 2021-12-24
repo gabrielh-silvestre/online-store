08d114dc29d363ef46208e6a06da2494e9e89bf9
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-24 20:44:07 -0300

    style(pages/index): add container

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,10 @@ import { Products } from '../src/components/Products';
 
 const Home: NextPage = () => {
   return (
-    <main className="relative bg-black px-4 pb-4">
-      <Products />
+    <main className="relative bg-black pb-8">
+      <div className="container">
+        <Products />
+      </div>
     </main>
   );
 };
