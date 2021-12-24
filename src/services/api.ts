86a4cb0b7ec7edec29847149956823fe07d15ea2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-24 19:54:53 -0300

    refactor(services): type axios output

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,29 +5,53 @@ interface SearchParams {
   category?: string;
 }
 
-export const apiListCategories = axios.create({
-  baseURL: 'https://api.mercadolibre.com/sites/MLB/',
-}).get('categories');
+interface SearchResponseData {
+  results: Array<{
+    id: string;
+    title: string;
+    price: number;
+    currency_id: string;
+    avaible_quantity: number;
+    thumbnail: string;
+    shipping: {
+      free_shipping: boolean;
+    };
+  }>;
+}
+
+export const apiListCategories = axios
+  .create({
+    baseURL: 'https://api.mercadolibre.com/sites/MLB/',
+  })
+  .get('categories');
 
 export const apiSearch = ({ term, category }: SearchParams) => {
   if (term && !category) {
-    return axios.create({
-      baseURL: 'https://api.mercadolibre.com/sites/MLB/',
-    }).get(`search?q=${term}`);
+    return axios
+      .create({
+        baseURL: 'https://api.mercadolibre.com/sites/MLB/',
+      })
+      .get<SearchResponseData | undefined>(`search?q=${term}`);
   }
 
   if (!term && category) {
-    return axios.create({
-      baseURL: 'https://api.mercadolibre.com/sites/MLB/',
-    }).get(`search?category=${category}`);
+    return axios
+      .create({
+        baseURL: 'https://api.mercadolibre.com/sites/MLB/',
+      })
+      .get<SearchResponseData | undefined>(`search?category=${category}`);
   }
 
-  return axios.create({
-    baseURL: 'https://api.mercadolibre.com/sites/MLB/',
-  }).get(`search?category=${category}&q=${term}`);
+  return axios
+    .create({
+      baseURL: 'https://api.mercadolibre.com/sites/MLB/',
+    })
+    .get<SearchResponseData | undefined>(`search?category=${category}&q=${term}`);
 };
 
 export const apiDetailItem = (productId: string) =>
-  axios.create({
-    baseURL: 'https://api.mercadolibre.com/items/',
-  }).get(productId);
+  axios
+    .create({
+      baseURL: 'https://api.mercadolibre.com/items/',
+    })
+    .get(productId);
