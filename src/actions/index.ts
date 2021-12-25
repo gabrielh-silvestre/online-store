f48ed522b6543ef11c33f28d760aa553860467cf
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-25 17:41:30 -0300

    feat(actions): create add cart action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,19 @@
 import { createAction } from '@reduxjs/toolkit';
 
+export interface CartItem {
+  id: string;
+  title: string;
+  price: number;
+  currency_id: string;
+  quantity: number;
+  thumbnail: string;
+  shipping: {
+    free_shipping: boolean;
+  };
+}
+
 export const setSearchTerm = createAction<string>('search/term');
 
 export const setCategory = createAction<string>('search/category');
+
+export const addCartItem = createAction<CartItem>('cart/add');
