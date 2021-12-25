adde2ceb6dc8d6e678ca805c53a49d3bbf68b006
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-24 21:05:24 -0300

    style(Products): make component scrollable

--- src/components/Products/styles.ts
+++ src/components/Products/styles.ts
@@ -1,13 +1,26 @@
+import styled from 'styled-components';
 import tw from 'tailwind-styled-components';
 
-export const Container = tw.article`
-  min-h-screen
+const ScrollContainer = styled.article`
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const Container = tw(ScrollContainer)`
+  min-h-full
+  max-h-screen
   p-8
 
   grid
   grid-cols-4
   auto-rows-min
   gap-4
+
+  overflow-y-auto
 
   rounded-md
 
