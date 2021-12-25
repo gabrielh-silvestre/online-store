22eaa4cc6a9a077bb49449873e8d0e52ea36aa53
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-25 01:14:00 -0300

    feat(_app): use progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import store from '../src/store';
 
 import { Header } from '../src/components/Header';
+import NextNProgress from 'nextjs-progressbar';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <NextNProgress />
       <Header />
       <Component {...pageProps} />
     </Provider>
