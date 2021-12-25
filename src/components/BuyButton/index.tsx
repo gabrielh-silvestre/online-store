0528ddc624dca0df0a96f358a855d4b5625268ca
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-25 17:47:59 -0300

    feat(ButButton): add cart item on click

--- src/components/BuyButton/index.tsx
+++ src/components/BuyButton/index.tsx
@@ -1,5 +1,34 @@
+import { useDispatch } from 'react-redux';
+
+import { addCartItem, CartItem } from '../../actions';
+
 import { Button } from './style';
 
-export function BuyButton() {
-  return <Button>Comprar</Button>;
+type BuyButtonProps = Omit<CartItem, 'quantity'>;
+
+export function BuyButton({
+  id,
+  title,
+  price,
+  currency_id,
+  shipping,
+  thumbnail,
+}: BuyButtonProps) {
+  const dispatch = useDispatch();
+
+  const handleAddCart = () => {
+    dispatch(
+      addCartItem({
+        id,
+        title,
+        price,
+        currency_id,
+        shipping,
+        thumbnail,
+        quantity: 1,
+      })
+    );
+  };
+
+  return <Button onClick={handleAddCart}>Comprar</Button>;
 }
