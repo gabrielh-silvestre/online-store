2cea8dddd169e6f2822531b43bc86c647534c1fe
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-27 16:24:15 -0300

    feat(components): create CartButton

---
+++ src/components/CartButton/index.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+import { CartButtonContainer } from './styles';
+
+interface CartButtonProps {
+  children: ReactNode;
+  isStrong?: boolean;
+  fontSize?: string;
+  onClick?: () => void;
+}
+
+export function CartButton({ children, isStrong, fontSize }: CartButtonProps) {
+  return (
+    <CartButtonContainer
+      $fontSize={fontSize}
+      $isStrong={isStrong}
+      type="button"
+    >
+      {children}
+    </CartButtonContainer>
+  );
+}
