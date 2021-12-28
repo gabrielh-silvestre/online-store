fd8257372764b14f9368f709299557e96d1b2278
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-28 16:01:09 -0300

    feat(CartItemCard): add increament and decreament item option

--- src/components/CartItemCard/index.tsx
+++ src/components/CartItemCard/index.tsx
@@ -1,4 +1,7 @@
+import { useDispatch } from 'react-redux';
 import Image from 'next/image';
+
+import { CartItem as CartItemCardProps, setQuantity } from '../../actions';
 
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
@@ -9,9 +12,19 @@ import {
   ProductQuantity,
 } from './styles';
 
-import { CartItem as CartItemCardProps } from '../../actions';
-
 export function CartItemCard(props: CartItemCardProps) {
+  const dispatch = useDispatch();
+
+  const handleArrowClick = (
+    id: string,
+    quantity: number,
+    increment?: boolean
+  ) => {
+    increment
+      ? dispatch(setQuantity({ id, quantity: (quantity += 1) }))
+      : dispatch(setQuantity({ id, quantity: (quantity -= 1) }));
+  };
+
   const formatPrice = () => {
     return new Intl.NumberFormat('pt-BR', {
       style: 'currency',
@@ -21,19 +34,24 @@ export function CartItemCard(props: CartItemCardProps) {
 
   return (
     <Container>
-      <Image
-        width={90}
-        height={90}
-        src={props.thumbnail}
-        alt={props.title}
-      />
+      <Image width={90} height={90} src={props.thumbnail} alt={props.title} />
       <ProductName>{props.title}</ProductName>
       <p className="text-xs text-center">
         Quant:
         <ProductQuantity>
-          <HiChevronLeft className="w-6 h-6 text-red" />
+          <HiChevronLeft
+            className="w-6 h-6 text-red"
+            onClick={() => {
+              handleArrowClick(props.id, props.quantity);
+            }}
+          />
           {props.quantity}
-          <HiChevronRight className="w-6 h-6 text-red" />
+          <HiChevronRight
+            className="w-6 h-6 text-red"
+            onClick={() => {
+              handleArrowClick(props.id, props.quantity, true);
+            }}
+          />
         </ProductQuantity>
       </p>
       <ProductPrice>{formatPrice()}</ProductPrice>
