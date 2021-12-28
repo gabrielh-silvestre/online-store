1b8cc0ce5de3af11e3918c3194c2e2cef2ce6988
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-28 14:59:06 -0300

    feat(components): create Bill component

---
+++ src/components/Bill/index.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { RootState } from '../../store';
+
+import { BillInfo, BillValue } from './styles';
+
+interface billState {
+  price: number;
+  shipping: number;
+  total: number;
+}
+
+export function Bill() {
+  const [bill, setBill] = useState<billState>({} as billState);
+  const cart = useSelector(({ cart }: RootState) => cart);
+
+  const formatPrice = (value: number) => {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(value);
+  };
+
+  useEffect(() => {
+    const cartItemsPrice = cart.reduce(
+      (acc, { price, shipping }) => {
+        if (shipping.free_shipping) acc.shipping = 14.99;
+        acc.price += price;
+        acc.total += price + acc.shipping;
+        return acc;
+      },
+      { price: 0, shipping: 0, total: 0 }
+    );
+
+    setBill(cartItemsPrice);
+  }, [cart]);
+
+  return (
+    <>
+      <BillInfo>
+        Valor: <BillValue>{formatPrice(bill.price)}</BillValue>
+      </BillInfo>
+      <BillInfo>
+        Frete: <BillValue>{formatPrice(bill.shipping)}</BillValue>
+      </BillInfo>
+      <BillInfo>
+        Total: <BillValue>{formatPrice(bill.total)}</BillValue>
+      </BillInfo>
+    </>
+  );
+}
