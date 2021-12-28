d60ee9d997894bb61138acd4891de78259f71cd3
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-28 14:34:39 -0300

    refactor(CartItems): cart page code for CartItems code

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,9 +1,60 @@
 import { NextPage } from 'next';
 
+import { CartButton } from '../src/components/CartButton';
 import { CartItems } from '../src/components/CartItems';
 
+import { Container, ContentContainer, CartItemsContainer, SectionTitle, CartBillContainer, BillInfo, BillValue, FinalPrice, CupomContainer, CupomInput } from '../styles/pageCart';
+
 const Cart: NextPage = () => {
-  return <CartItems />;
+  return (
+    <Container>
+    <div className="container">
+      <ContentContainer>
+        <CartItemsContainer>
+          <div className="grid grid-cols-3">
+            <SectionTitle>Produto</SectionTitle>
+            <div>
+              <CartButton fontSize="text-sm">
+                REMOVER TODOS OS PRODUTOS
+              </CartButton>
+            </div>
+          </div>
+
+          <div className="my-4">
+            <CartItems />
+          </div>
+
+          <CupomContainer>
+            <CupomInput type="text" placeholder="Cupom de desconto" />
+            <CartButton isStrong>Aplicar cupom</CartButton>
+          </CupomContainer>
+        </CartItemsContainer>
+
+        <CartBillContainer>
+          <SectionTitle>Resumo</SectionTitle>
+
+          <div className="my-4">
+            <BillInfo>
+              Valor: <BillValue>R$ 200,00</BillValue>
+            </BillInfo>
+            <BillInfo>
+              Frete: <BillValue>R$ 200,00</BillValue>
+            </BillInfo>
+            <BillInfo>
+              Total: <BillValue>R$ 200,00</BillValue>
+            </BillInfo>
+          </div>
+
+          <FinalPrice>R$200,00</FinalPrice>
+
+          <div className="h-12">
+            <CartButton isStrong>Finalizar</CartButton>
+          </div>
+        </CartBillContainer>
+      </ContentContainer>
+    </div>
+  </Container>
+  );
 };
 
 export default Cart;
--- src/components/CartItems/index.tsx
+++ src/components/CartItems/index.tsx
@@ -1,69 +1,16 @@
-import { CartButton } from '../CartButton';
+import { RootState } from '../../store';
+
+import { useSelector } from 'react-redux';
 import { CartItemCard } from '../CartItemCard';
 
-import {
-  BillInfo,
-  BillValue,
-  CartBillContainer,
-  CartItemsContainer,
-  Container,
-  ContentContainer,
-  CupomContainer,
-  CupomInput,
-  FinalPrice,
-  SectionTitle,
-} from './styles';
-
 export function CartItems() {
+  const cart = useSelector(({ cart }: RootState) => cart);
+
   return (
-    <Container>
-      <div className="container">
-        <ContentContainer>
-          <CartItemsContainer>
-            <div className="grid grid-cols-3">
-              <SectionTitle>Produto</SectionTitle>
-              <div>
-                <CartButton fontSize="text-sm">
-                  REMOVER TODOS OS PRODUTOS
-                </CartButton>
-              </div>
-            </div>
-
-            <div className="my-4">
-              <CartItemCard />
-              <CartItemCard />
-              <CartItemCard />
-            </div>
-
-            <CupomContainer>
-              <CupomInput type="text" placeholder="Cupom de desconto" />
-              <CartButton isStrong>Aplicar cupom</CartButton>
-            </CupomContainer>
-          </CartItemsContainer>
-
-          <CartBillContainer>
-            <SectionTitle>Resumo</SectionTitle>
-
-            <div className="my-4">
-              <BillInfo>
-                Valor: <BillValue>R$ 200,00</BillValue>
-              </BillInfo>
-              <BillInfo>
-                Frete: <BillValue>R$ 200,00</BillValue>
-              </BillInfo>
-              <BillInfo>
-                Total: <BillValue>R$ 200,00</BillValue>
-              </BillInfo>
-            </div>
-
-            <FinalPrice>R$200,00</FinalPrice>
-
-            <div className="h-12">
-              <CartButton isStrong>Finalizar</CartButton>
-            </div>
-          </CartBillContainer>
-        </ContentContainer>
-      </div>
-    </Container>
+    <>
+      {cart.map((item) => (
+        <CartItemCard key={item.id} {...item} />
+      ))}
+    </>
   );
 }
