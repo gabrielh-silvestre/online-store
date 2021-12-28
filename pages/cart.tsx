731f4e4c543eb00109bdb2ef5f419c03a09baa91
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-28 15:00:07 -0300

    refactor(pages/cart): use external component to show bill

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,59 +1,61 @@
 import { NextPage } from 'next';
+import { Bill } from '../src/components/Bill';
 
 import { CartButton } from '../src/components/CartButton';
 import { CartItems } from '../src/components/CartItems';
 
-import { Container, ContentContainer, CartItemsContainer, SectionTitle, CartBillContainer, BillInfo, BillValue, FinalPrice, CupomContainer, CupomInput } from '../styles/pageCart';
+import {
+  Container,
+  ContentContainer,
+  CartItemsContainer,
+  SectionTitle,
+  CartBillContainer,
+  FinalPrice,
+  CupomContainer,
+  CupomInput,
+} from '../styles/pageCart';
 
 const Cart: NextPage = () => {
   return (
     <Container>
-    <div className="container">
-      <ContentContainer>
-        <CartItemsContainer>
-          <div className="grid grid-cols-3">
-            <SectionTitle>Produto</SectionTitle>
-            <div>
-              <CartButton fontSize="text-sm">
-                REMOVER TODOS OS PRODUTOS
-              </CartButton>
+      <div className="container">
+        <ContentContainer>
+          <CartItemsContainer>
+            <div className="grid grid-cols-3">
+              <SectionTitle>Produto</SectionTitle>
+              <div>
+                <CartButton fontSize="text-sm">
+                  REMOVER TODOS OS PRODUTOS
+                </CartButton>
+              </div>
             </div>
-          </div>
 
-          <div className="my-4">
-            <CartItems />
-          </div>
+            <div className="my-4">
+              <CartItems />
+            </div>
 
-          <CupomContainer>
-            <CupomInput type="text" placeholder="Cupom de desconto" />
-            <CartButton isStrong>Aplicar cupom</CartButton>
-          </CupomContainer>
-        </CartItemsContainer>
+            <CupomContainer>
+              <CupomInput type="text" placeholder="Cupom de desconto" />
+              <CartButton isStrong>Aplicar cupom</CartButton>
+            </CupomContainer>
+          </CartItemsContainer>
 
-        <CartBillContainer>
-          <SectionTitle>Resumo</SectionTitle>
+          <CartBillContainer>
+            <SectionTitle>Resumo</SectionTitle>
 
-          <div className="my-4">
-            <BillInfo>
-              Valor: <BillValue>R$ 200,00</BillValue>
-            </BillInfo>
-            <BillInfo>
-              Frete: <BillValue>R$ 200,00</BillValue>
-            </BillInfo>
-            <BillInfo>
-              Total: <BillValue>R$ 200,00</BillValue>
-            </BillInfo>
-          </div>
+            <div className="my-4">
+              <Bill />
+            </div>
 
-          <FinalPrice>R$200,00</FinalPrice>
+            <FinalPrice>R$200,00</FinalPrice>
 
-          <div className="h-12">
-            <CartButton isStrong>Finalizar</CartButton>
-          </div>
-        </CartBillContainer>
-      </ContentContainer>
-    </div>
-  </Container>
+            <div className="h-12">
+              <CartButton isStrong>Finalizar</CartButton>
+            </div>
+          </CartBillContainer>
+        </ContentContainer>
+      </div>
+    </Container>
   );
 };
 
