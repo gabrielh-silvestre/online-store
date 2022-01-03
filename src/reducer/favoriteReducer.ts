4a7b14c17f8aaeacaddd5553a1c0b47dfc103937
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-01-03 20:26:30 -0300

    feat(favoriteReducer): add toast warn in cases

--- src/reducer/favoriteReducer.ts
+++ src/reducer/favoriteReducer.ts
@@ -1,4 +1,5 @@
 import { createReducer } from '@reduxjs/toolkit';
+import toast from 'react-hot-toast';
 
 import { addFavoriteItem, FavoriteItem, removeFavoriteItem } from '../actions';
 
@@ -6,11 +7,21 @@ const INITIAL_STATE = [] as Array<FavoriteItem>;
 
 export const FavoriteReducer = createReducer(INITIAL_STATE, (builder) => {
   builder.addCase(addFavoriteItem, (state, { payload }) => {
+    toast.success(`${payload.title} adicionado aos favoritos`, {
+      id: payload.id,
+      className: 'max-h-20 py-2 overflow-clip hover:overflow-y-auto',
+    });
+
     state.push(payload);
   });
 
   builder.addCase(removeFavoriteItem, (state, { payload }) => {
-    const favoriteIndex = state.findIndex((item) => item.id === payload);
+    toast.error(`${payload.title} removido dos favoritos`, {
+      id: payload.id,
+      className: 'max-h-20 py-2 overflow-clip hover:overflow-y-auto',
+    });
+
+    const favoriteIndex = state.findIndex((item) => item.id === payload.id);
     state.splice(favoriteIndex, 1);
   });
 });
