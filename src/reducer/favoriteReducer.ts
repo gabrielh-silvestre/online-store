862ea65b170f2dfb6c14c346dfd54eb925263f55
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-01-03 17:40:21 -0300

    feat(reducer): implement remove favorite case

--- src/reducer/favoriteReducer.ts
+++ src/reducer/favoriteReducer.ts
@@ -1,11 +1,16 @@
 import { createReducer } from '@reduxjs/toolkit';
 
-import { addFavoriteItem, FavoriteItem } from '../actions';
+import { addFavoriteItem, FavoriteItem, removeFavoriteItem } from '../actions';
 
 const INITIAL_STATE = [] as Array<FavoriteItem>;
 
 export const FavoriteReducer = createReducer(INITIAL_STATE, (builder) => {
   builder.addCase(addFavoriteItem, (state, { payload }) => {
     state.push(payload);
+  });
+
+  builder.addCase(removeFavoriteItem, (state, { payload }) => {
+    const favoriteIndex = state.findIndex((item) => item.id === payload);
+    state.splice(favoriteIndex, 1);
   });
 });
